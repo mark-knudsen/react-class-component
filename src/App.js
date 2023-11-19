41d349c61b00a5a4bdc652f4e951d1a2567d9237
mark-knudsen <marksvendstrupknudsen@gmail.com>
2023-11-19 17:23:37 +0100

    added todo input and delete function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+      value: "",
+    };
+  }
+  onChange = (e) => {
+    this.setState({ value: e.target.value });
+  };
+  onAddTask = (e) => {
+    e.preventDefault();
+
+    const obj = {
+      name: this.state.value,
+      id: Date.now(),
+    };
+    if (this.state.value !== "") {
+      this.setState({ todos: this.state.todos.concat(obj) });
+      this.setState({ value: "" });
+    }
+  };
+
+  onDeleteTask = (itemId) => {
+    this.setState({
+      todos: [...this.state.todos].filter((id) => id.id !== itemId),
+    });
+  };
+
+  render() {
+    const mylist = this.state.todos.map((todo) => (
+      <li className="todo_item">
+        {todo.name}
+
+        <button>Edit</button>
+        <button onClick={() => this.onDeleteTask(todo.id)}>Remove</button>
+      </li>
+    ));
+
+    return (
+      <>
+        <div className="App">
+          <form onSubmit={this.onAddTask}>
+            <input
+              placeholder="typeyour task"
+              value={this.state.value}
+              onChange={this.onChange}
+            />
+            <button onClick={this.onAddTask}>Add Item</button>
+          </form>
+
+          <ul className="todo_wrapper">{mylist}</ul>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
